201fa64d5aff0bc971dbc1d5c7d6a95357ee2402
Jaehun <qkdwogns98@gmail.com>
2025-01-27 00:10:58 +0900

    module ...

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,14 @@ import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 
 @Module({
+  // imports 는 다른 모듈을 가져온다.
+  // cli 를 사용하면 자동으로 설정됨.
   imports: [PostsModule],
+
   controllers: [AppController],
   providers: [AppService],
 })
+
+// 그럼 imports 하는게 없는 AppModule 은 등록된지 어떻게 알지?
+// main.ts
 export class AppModule {}
