8660ff5d44bb3b44c0c674983394ec8c7a3f4d04
Jaehun <qkdwogns98@gmail.com>
2025-01-28 20:49:52 +0900

    login, register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    return this.authService.loginWithEmail({
+      email,
+      password,
+    });
+  }
+
+  @Post('register')
+  async register(
+    @Body('email') email: string,
+    @Body('password') password: string,
+    @Body('nickname') nickname: string,
+  ) {
+    return this.authService.registerWithEmail({
+      email,
+      password,
+      nickname,
+    });
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,87 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import bycrypt from 'bcrypt';
+
+import { JWT_SECRET } from './const/auth.const';
+
+export interface User {
+  email: string;
+  password: string;
+}
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  /**
+   * payload (1) email (2) sub - id (3) type - access_token | refresh_token
+   *
+   * Pick<Model> 은 Model 의 속성. 이라는 문맥을 나타내서 좋다.
+   */
+  signToken(user: User, isRefreshToken: boolean) {
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      type: isRefreshToken ? 'refresh_token' : 'access_token',
+    };
+
+    this.jwtService.sign(payload, {
+      // FIXME: to env variable
+      secret: JWT_SECRET,
+
+      // sec
+      expiresIn: isRefreshToken ? 3600 : 360,
+    });
+
+    return this.jwtService.sign(payload);
+  }
+
+  loginUser(user: User) {
+    const accessToken = this.signToken(user, false);
+    const refreshToken = this.signToken(user, true);
+
+    return { accessToken, refreshToken };
+  }
+
+  async authenticateWithEmailAndPassword(user: User) {
+    // TODO: UserModule 로 검증
+
+    const exsitingUser = await this.userService.findByEmail(user.email);
+
+    if (!exsitingUser) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    // bycrypt 로 비밀번호 검증
+    // password - original
+    // exsitingUser.password - hashed
+    if (bycrypt.compare(user.password, exsitingUser.password)) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    return exsitingUser;
+  }
+
+  async loginWithEmail(user: User) {
+    const exsitingUser = await this.authenticateWithEmailAndPassword(user);
+
+    return this.loginUser(exsitingUser);
+  }
+
+  async registerWithEmail(user: User & { nickname: string }) {
+    // hash round: 10
+    // round=10 = 초당 10번 = 10 hashes/seconds
+    const hashedPassword = await bycrypt.hash(user.password, 10);
+
+    // bycrypt.hash() = salt 는 자동생성됨.
+
+    // TODO: createUser()
+    // userRepository.exist() 로 존재 여부 검증 (nickname / email)
+    const newUser = await this.userService.createUser({
+      ...user,
+      password: hashedPassword,
+    });
+
+    return this.loginUser(newUser);
+  }
+}
