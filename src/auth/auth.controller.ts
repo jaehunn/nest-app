3a952d6b9b2c242e9e2116826737edae3aa4ebe3
Jaehun <qkdwogns98@gmail.com>
2025-01-28 21:36:54 +0900

    access / refresh

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,64 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Headers, Post } from '@nestjs/common';
+
 import { AuthService } from './auth.service';
+
+// access token -> expired -> rotate token -> return access token
+// refresh token -> expired -> rotate token -> return refresh token
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('login')
-  async login(
-    @Body('email') email: string,
-    @Body('password') password: string,
-  ) {
-    return this.authService.loginWithEmail({
-      email,
-      password,
-    });
+  @Post('login/email')
+  async loginEmail(@Headers('authorization') rawToken: string) {
+    // 1. token extract
+    const token = await this.authService.extractTokenFromHeader(
+      rawToken,
+      false,
+    );
+
+    // 2. parsing token
+    const credentials = await this.authService.decodeBasicToken(token);
+
+    // 3. login
+    return this.authService.loginWithEmail(credentials);
   }
+
+  @Post('token/access')
+  async tokenAccess(@Headers('authorization') rawToken: string) {
+    // 1. token extract (bearer)
+    const token = await this.authService.extractTokenFromHeader(rawToken, true);
+
+    // 2. rotate token
+    const newToken = this.authService.rotateToken(token, false);
+
+    return {
+      accessToken: newToken,
+    };
+  }
+  @Post('token/refresh')
+  async tokenRefresh(@Headers('authorization') rawToken: string) {
+    // 1. token extract (bearer)
+    const token = await this.authService.extractTokenFromHeader(rawToken, true);
+
+    // 2. rotate token
+    const newToken = this.authService.rotateToken(token, true);
+
+    return {
+      refreshToken: newToken,
+    };
+  }
+
+  // @Post('login')
+  // async login(
+  //   @Body('email') email: string,
+  //   @Body('password') password: string,
+  // ) {
+  //   return this.authService.loginWithEmail({
+  //     email,
+  //     password,
+  //   });
+  // }
 
   @Post('register')
   async register(
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,12 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
 import bycrypt from 'bcrypt';
 
 import { JWT_SECRET } from './const/auth.const';
 
 export interface User {
+  id: string;
   email: string;
   password: string;
 }
@@ -14,11 +16,58 @@ export class AuthService {
   constructor(private readonly jwtService: JwtService) {}
 
   /**
+   * 인가 프로세스
+   * 1. 로그인/회원가입시 acc / ref token 발급
+   * 2. basic auth 형태로 인증 (username:password + base64)
+   * 3. * 인가가 적용되어야하는 api route 에 (bearer + access token)
+   * 4. token 검증으로 사용자가 누구인지 파악가능 sub 값으로 프로필 추출 가능
+   * 5. expire 된다. verify() 에서 인증 미통과됨. access token 토큰 재발급 로직 필요.
+   *
+   * basic 이거나 bearer 면 토큰 추출
+   */
+  async extractTokenFromHeader(header: string, isBasic: boolean) {
+    const [type, token] = header?.split(' ') ?? [];
+
+    if (type === 'Basic' || type === 'Bearer') {
+      return token ?? null;
+    }
+
+    return undefined;
+  }
+
+  async decodeBasicToken(token: string) {
+    // TODO: Buffer.from()
+    const decoded = Buffer.from(token, 'base64').toString('utf-8');
+
+    const [email, password] = decoded.split(':');
+
+    return { email, password };
+  }
+
+  async verifyToken(token: string) {
+    return this.jwtService.verify(token, {
+      secret: JWT_SECRET,
+    });
+  }
+
+  // 새로 발급
+  rotateToken(token: string, isRefreshToken: boolean) {
+    const decoded = this.verifyToken(token);
+
+    // sub, email, type
+    if (decoded?.type !== 'refresh_token') {
+      throw new UnauthorizedException('Invalid token');
+    }
+
+    return this.signToken(...decoded, isRefreshToken);
+  }
+
+  /**
    * payload (1) email (2) sub - id (3) type - access_token | refresh_token
    *
    * Pick<Model> 은 Model 의 속성. 이라는 문맥을 나타내서 좋다.
    */
-  signToken(user: User, isRefreshToken: boolean) {
+  signToken(user: Pick<User, 'email' | 'password'>, isRefreshToken: boolean) {
     const payload = {
       email: user.email,
       sub: user.id,
@@ -43,7 +92,9 @@ export class AuthService {
     return { accessToken, refreshToken };
   }
 
-  async authenticateWithEmailAndPassword(user: User) {
+  async authenticateWithEmailAndPassword(
+    user: Pick<User, 'email' | 'password'>,
+  ) {
     // TODO: UserModule 로 검증
 
     const exsitingUser = await this.userService.findByEmail(user.email);
@@ -62,13 +113,15 @@ export class AuthService {
     return exsitingUser;
   }
 
-  async loginWithEmail(user: User) {
+  async loginWithEmail(user: Pick<User, 'email' | 'password'>) {
     const exsitingUser = await this.authenticateWithEmailAndPassword(user);
 
     return this.loginUser(exsitingUser);
   }
 
-  async registerWithEmail(user: User & { nickname: string }) {
+  async registerWithEmail(
+    user: Pick<User, 'email' | 'password'> & { nickname: string },
+  ) {
     // hash round: 10
     // round=10 = 초당 10번 = 10 hashes/seconds
     const hashedPassword = await bycrypt.hash(user.password, 10);
